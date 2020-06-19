ccc50e25d99662992bec387ec2f7c4bdbfc7f2e6
jujuas <34321418+jujuas@users.noreply.github.com>
2020-06-18 21:57:55 -0300

    Ajustando novamente

--- public/start-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose down
-ip=$(hostname -I | awk '{print $1}')
-echo "nginx sendo configurado com o ip $ip";
-sed "s/server [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+:/server $ip:/" -i ../docker/nginx/nginx.conf
-docker-compose up -d
